3545f4e6eb4f44f17c93557f424bfe4873d11825
Ben Edmunds <sammybenblue2@yahoo.co.uk>
2018-09-21 16:51:53 +0100

    Update for monitoring multiple servers

--- docker/speedtest/run.sh
+++ docker/speedtest/run.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
 while :; do
-	echo "[Info][$(date)] Starting speedtest..."
-	JSON=$(speed-test -j)
+	echo "[Info][$(date)] Starting speedtest... to closest server"
+	JSON=$(speed-test -j -v)
 	DOWNLOAD=$(echo "${JSON}" | json download)
 	UPLOAD=$(echo "$JSON" | json upload)
 	PING=$(echo "${JSON}" | json ping)
-	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}"
-	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "download,host=${SPEEDTEST_HOST} value=${DOWNLOAD}"
-	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "upload,host=${SPEEDTEST_HOST} value=${UPLOAD}"
-	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "ping,host=${SPEEDTEST_HOST} value=${PING}"
+	HOST=$(echo "${JSON}" | json data | json server | json host)
+	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}, Host: ${HOST}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "download,host=${HOST} value=${DOWNLOAD}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "upload,host=${HOST} value=${UPLOAD}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "ping,host=${HOST} value=${PING}"
+	echo "sleeping before nexttest"
+	sleep 10
+	echo "[Info][$(date)] Starting speedtest... to CFL"
+	JSON=$(speed-test -j -v -s CFL)
+	DOWNLOAD=$(echo "${JSON}" | json download)
+	UPLOAD=$(echo "$JSON" | json upload)
+	PING=$(echo "${JSON}" | json ping)
+	HOST=$(echo "${JSON}" | json data | json server | json host)
+	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}, Host: ${HOST}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "download,host=${HOST} value=${DOWNLOAD}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "upload,host=${HOST} value=${UPLOAD}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "ping,host=${HOST} value=${PING}"
+	echo "sleeping before next test"
+	sleep 10
+	echo "[Info][$(date)] Starting speedtest... to Wimbledon"
+	JSON=$(speed-test -j -v -s Wimbledon)
+	DOWNLOAD=$(echo "${JSON}" | json download)
+	UPLOAD=$(echo "$JSON" | json upload)
+	PING=$(echo "${JSON}" | json ping)
+	HOST=$(echo "${JSON}" | json data | json server | json host)
+	echo "[Info][$(date)] Speedtest results - Download: ${DOWNLOAD}, Upload: ${UPLOAD}, Ping: ${PING}, Host: ${HOST}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "download,host=${HOST} value=${DOWNLOAD}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "upload,host=${HOST} value=${UPLOAD}"
+	curl -sL -XPOST "${INFLUXDB_URL}/write?db=${INFLUXDB_DB}" --data-binary "ping,host=${HOST} value=${PING}"
 	echo "[Info][$(date)] Sleeping for ${SPEEDTEST_INTERVAL} seconds..."
 	sleep ${SPEEDTEST_INTERVAL}
+	echo "sleeping before second test"
 done
